a0c143318303f735d2553c0c9a555ca5704b8c15
Vani Narayana <codez4future@gmail.com>
2016-10-12 14:03:31 -0400

    Revert "Revert "Multiple changes""
    This reverts commit 06d83e04a4907377b5cf1203a3d121acd1ffe773.

--- wp-content/themes/spasalon/footer.php
+++ wp-content/themes/spasalon/footer.php
@@ -1,71 +1,71 @@
-<?php 
+<?php
 $current_options = wp_parse_args(  get_option( 'spa_theme_options', array() ), default_data() );
 ?>
 <!-- Footer Section -->
-<footer class="footer-sidebar">	
-	
+<footer class="footer-sidebar">
+
 	<?php if( is_active_sidebar('footer-sidebar1') || is_active_sidebar('footer-sidebar2') || is_active_sidebar('footer-sidebar3') || is_active_sidebar('footer-sidebar4')) : ?>
 	<div class="empty-footer-sidebar">
-	<!-- Footer Widgets -->	
-	<div class="container">		
-		<div class="row">		
-			
-			<div class="col-md-3 col-sm-6">
-				<?php 
+	<!-- Footer Widgets -->
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-5 col-sm-6">
+				<?php
 				if( is_active_sidebar('footer-sidebar1') ) :
-					dynamic_sidebar( 'footer-sidebar1' ); 
+					dynamic_sidebar( 'footer-sidebar1' );
 				endif;
 				?>
 			</div>
-					
-			<div class="col-md-3 col-sm-6">		
-				<?php 
+
+			<div class="col-md-2 col-sm-6">
+				<?php
 				if( is_active_sidebar('footer-sidebar2') ) :
-					dynamic_sidebar( 'footer-sidebar2' ); 
+					dynamic_sidebar( 'footer-sidebar2' );
 				endif;
 				?>
-			</div>	
-			
-			<div class="col-md-3 col-sm-6">	
-				<?php 
+			</div>
+
+			<div class="col-md-5 col-sm-6">
+				<?php
 				if( is_active_sidebar('footer-sidebar3') ) :
-					dynamic_sidebar( 'footer-sidebar3' ); 
+					dynamic_sidebar( 'footer-sidebar3' );
 				endif;
 				?>
 			</div>
-			
-			<div class="col-md-3 col-sm-6">
-				<?php 
+
+			<!-- <div class="col-md-3 col-sm-6">
+				<?php
 				if( is_active_sidebar('footer-sidebar4') ) :
-					dynamic_sidebar( 'footer-sidebar4' ); 
+					dynamic_sidebar( 'footer-sidebar4' );
 				endif;
 				?>
-			</div>
-			
+			</div> -->
+
 		</div>
 	</div>
-	<!-- /End of Footer Widgets -->	
+	<!-- /End of Footer Widgets -->
 	</div>
 	<?php endif; ?>
-	
-	<!-- Copyrights -->	
+
+	<!-- Copyrights -->
 	<div class="site-info">
 		<div class="container">
 			<div class="row">
-			
+
 				<div class="col-md-7">
-				
+
 					<?php if( $current_options['footer_tagline'] ){ ?>
 					<p><?php echo wp_kses_post($current_options['footer_tagline']); ?></p>
 					<?php } ?>
-					
+
 				</div>
-				
+
 				<div class="col-md-5">
 					<ul class="footer-links">
-					  <?php  
-						wp_nav_menu( 
-							array(  
+					  <?php
+						wp_nav_menu(
+							array(
 								'menu'           => 'footer-menu',
 								'theme_location' => ( has_nav_menu( 'footer' ) ? 'footer' : 'primary' ),
 								'depth'          => -1,
@@ -79,18 +79,18 @@ $current_options = wp_parse_args(  get_option( 'spa_theme_options', array() ), d
 						?>
 					</ul>
 				</div>
-				
+
 			</div>
 		</div>
 	</div>
-	<!-- Copyrights -->	
-	
+	<!-- Copyrights -->
+
 </footer>
 <!-- /End of Footer Section -->
 
-<!--Scroll To Top--> 
+<!--Scroll To Top-->
 <a href="#" class="scrollup"><i class="fa fa-chevron-up"></i></a>
-<!--/End of Scroll To Top--> 	
+<!--/End of Scroll To Top-->
 <?php wp_footer(); ?>
 </body>
 </html>
--- wp-content/themes/spasalon/js/custom.js
+++ wp-content/themes/spasalon/js/custom.js
@@ -1,19 +1,19 @@
 /*Menu Dropdown Onhover Js*/
- 
+
 	jQuery(document).ready(function() {
 	   jQuery('.nav li.dropdown').hover(function() {
 		   jQuery(this).addClass('open');
 	   }, function() {
 		   jQuery(this).removeClass('open');
-	   }); 
-	   
+	   });
+
 	});
-	
-	  
-	
+
+
+
 /* Page Scroll onclick button Js */
 	jQuery(document).ready(function () {
-	
+
 		jQuery(window).scroll(function () {
 			if (jQuery(this).scrollTop() > 100) {
 				jQuery('.scrollup').fadeIn();
@@ -21,12 +21,18 @@
 				jQuery('.scrollup').fadeOut();
 			}
 		});
-	
+
 		jQuery('.scrollup').click(function () {
 			jQuery("html, body").animate({
 				scrollTop: 0
 			}, 500);
 			return false;
 		});
-	
+
 	});
+
+		jQuery(document).ready(function () {
+			jQuery('.flexslider').flexslider({
+					animation:"fade",
+					});
+		});
