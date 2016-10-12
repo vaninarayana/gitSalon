4aaad0c5a93d96c6c997182cedac6f4f9613cb23
Vani Narayana <codez4future@gmail.com>
2016-10-12 14:17:03 -0400

    Scripts.php
    Added js for Flexslider

--- wp-content/themes/spasalon/functions/scripts/script.php
+++ wp-content/themes/spasalon/functions/scripts/script.php
@@ -3,24 +3,25 @@
 if( !function_exists('spasalon_scripts'))
 {
 	function spasalon_scripts(){
-		
+
 		$current_options = wp_parse_args(  get_option( 'spa_theme_options', array() ), default_data() );
-		
+
 		// css
 		wp_enqueue_style('style', get_stylesheet_uri() );
 		wp_enqueue_style('bootstrap', WEBRITI_TEMPLATE_DIR_URI . '/css/bootstrap.css' );
 		wp_enqueue_style('custom', WEBRITI_TEMPLATE_DIR_URI . '/css/custom.css' );
 		wp_enqueue_style('default', WEBRITI_TEMPLATE_DIR_URI . '/css/default.css' );
 		wp_enqueue_style('flexslider', WEBRITI_TEMPLATE_DIR_URI . '/css/flexslider.css' );
-		
+
 		wp_enqueue_style('spasalon-font', WEBRITI_TEMPLATE_DIR_URI . '/css/font/font.css' );
 		wp_enqueue_style('spasalon-font-awesome', WEBRITI_TEMPLATE_DIR_URI . '/css/font-awesome/css/font-awesome.min.css' );
-		
+
 		// js
 		wp_enqueue_script( 'jquery' );
+		wp_enqueue_script( 'spasalon-bootstrap-js' , WEBRITI_TEMPLATE_DIR_URI . '/js/jquery.flexslider-min.js' );
 		wp_enqueue_script( 'spasalon-bootstrap-js' , WEBRITI_TEMPLATE_DIR_URI . '/js/bootstrap.min.js' );
 		wp_enqueue_script( 'spasalon-custom-js' , WEBRITI_TEMPLATE_DIR_URI . '/js/custom.js' );
-		
+
 		if ( is_singular() ) wp_enqueue_script( "comment-reply" );
 	}
 }
